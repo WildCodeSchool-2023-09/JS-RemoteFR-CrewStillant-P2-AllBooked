e1e18c1701a948dde785af47a02de9aaf59bb6c9
Halicksse <alix.carlhant.pro@gmail.com>
2023-11-10 10:21:59 +0100

    majfooter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./App.css";
-// import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* <SearchBar /> */}
+
       <Outlet />
       <Footer />
     </div>
--- frontend/src/components/SearchBar.jsx
+++
@@ -1,5 +0,0 @@
-// import React from "react";
-
-// export const searchBar2 = () => {
-//   return <div>searchBar2</div>;
-// };
