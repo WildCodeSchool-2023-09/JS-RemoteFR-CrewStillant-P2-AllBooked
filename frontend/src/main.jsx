f3880816c8db2aa22000abfab7889517df29b41c
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-11-11 19:10:50 +0100

    Still working, update my local dev

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Carousel from "./components/Carousel";
+import Recherche from "./pages/Recherche";
 
 const router = createBrowserRouter([
   {
@@ -13,12 +14,21 @@ const router = createBrowserRouter([
         element: <Carousel />,
       },
       {
+        path: "recherche/:search",
+        element: <Recherche />,
+        loader: ({ params }) => {
+          fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=${params.search}&printType=books&key=AIzaSyCRqS28SI90ab1CXYQ0DUmXdL_P_vRGVEM&maxResults=40`
+          );
+        },
+      },
+      {
         path: "/bookdescription",
         element: "",
       },
       {
         path: "*",
-        element: "<h1>Page not found sorry ¯_(ツ)_/¯</h1>",
+        element: <h1>Page not found sorry ¯_(ツ)_/¯</h1>,
       },
     ],
   },
